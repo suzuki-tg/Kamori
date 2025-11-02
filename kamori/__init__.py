fcdb3f49ddab28506b1e1a71378b72cffd7e00a1
suzuki-team <166751167+suzuki-tg@users.noreply.github.com>
2025-11-02 23:50:17 +0300

    Create __init__.py

---
+++ kamori/__init__.py
@@ -0,0 +1,16 @@
+"""🎄 Just a placeholder to do relative imports 🎄"""
+# ©️ Kamori, 2021-2025
+# This file is a part of Kamori Userbot
+# 🌐 https://github.com/suzuki-tl/Kamori
+# You can redistribute it and/or modify it under the terms of the GNU AGPLv3
+# 🔑 https://www.gnu.org/licenses/agpl-3.0.html
+
+# Do not delete this file, it will cause errors.
+
+__author__ = "Kamori"
+__contact__ = "me@kamori-team.ru"
+__copyright__ = "Copyright 2022-2025, Kamori"
+__credits__ = ["Kamori Team"]
+__license__ = "AGPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
